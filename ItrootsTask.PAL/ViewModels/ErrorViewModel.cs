1b484d4b0354007295ee5ded79f21ae7c370b0c1
badrsaeed <55393290+badrsaeed@users.noreply.github.com>
2023-06-14 17:15:14 +0300

    Initial Commit that creating project

---
+++ ItrootsTask.PAL/ViewModels/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ItrootsTask.PAL.ViewModels
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
